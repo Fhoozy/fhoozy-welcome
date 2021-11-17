2190c2c89bfe36a39f5bfb65303998c26070f89b
Fhoozy <75787893+Fhoozy@users.noreply.github.com>
2021-11-17 09:49:55 +0300

    Update fhoozy.js

--- fhoozy.js
+++ fhoozy.js
@@ -1,23 +1,25 @@
 const { Client } = require("discord.js");
-const { channels, staffRoles, unregisterRoles, fhoozyunregisterSound, fhoozystaffSound, tokens } = require("./settings.json");
+const { channels, staffRoles, unregisterRoles, fhoozyunregisterSound, fhoozystaffSound, tokens } = require("./settings.json");   //Created By. Fhoozy Lyrocks (Fhoozy#0001)
 
  tokens.forEach((token, i) => {
   const client = new Client();
   let connection;
-  client.on("ready", async () => connection = await client.channels.cache.get(channels[i]).join());
+  client.on("ready", async () => connection = await client.channels.cache.get(channels[i]).join());   //Created By. Fhoozy Lyrocks (Fhoozy#0001)
     
   client.on("voiceStateUpdate", async (oldState, newState) => {
-    if ((oldState.channelID && !newState.channelID) || (oldState.channelID && newState.channelID && oldState.channelID === newState.channelID) || newState.member.user.bot || newState.channelID !== channels[i]) return;
+    if ((oldState.channelID && !newState.channelID) || (oldState.channelID && newState.channelID && oldState.channelID === newState.channelID) || newState.member.user.bot || newState.channelID !== channels[i]) return;   //Created By. Fhoozy Lyrocks (Fhoozy#0001)
     const hasStaff = newState.channel.members.some((x)=> staffRoles.some((r) => x.roles.cache.has(r)));
-    const staffSize = newState.channel.members.filter((x) => staffRoles.some((r) => x.roles.cache.has(r))).size;
+    const staffSize = newState.channel.members.filter((x) => staffRoles.some((r) => x.roles.cache.has(r))).size;   //Created By. Fhoozy Lyrocks (Fhoozy#0001)
     const unregisterSize = newState.channel.members.filter((x) => unregisterRoles.some((r) => x.roles.cache.has(r))).size;
     if (!hasStaff && unregisterSize === 1) await connection.play(fhoozyunregisterSound);
-    else if (hasStaff && staffSize === 1 && unregisterSize === 1) await connection.play(fhoozystaffSound);
+    else if (hasStaff && staffSize === 1 && unregisterSize === 1) await connection.play(fhoozystaffSound);   //Created By. Fhoozy Lyrocks (Fhoozy#0001)
   });
 
   
   client.on("ready", async () => {
-  client.user.setPresence({ activity: { name: "Rhovanion ❤️ Fhoozy" }, status: "idle" });
+  client.user.setPresence({ activity: { name: "Sunucu Adınız ❤️ İsminiz" }, status: "idle" }); //Created By. Fhoozy Lyrocks (Fhoozy#0001)
   })
-  client.login(token).then(() => console.log(`${client.user.tag} Aktif!`)).catch(() => console.error(`${token} Tokeni aktif edilemedi!`));
-});
+  
+  
+  client.login(token).then(() => console.log(`${client.user.tag} Aktif!`)).catch(() => console.error(`${token} | Tokende Hata Var, Token Aktif Edilemedi!`));
+});//Created By. Fhoozy Lyrocks (Fhoozy#0001)
